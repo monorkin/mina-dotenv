f1f47308fe6fe251541b575a8854120ff7fe7249
Stanko Krtalić <stanko.krtalic@gmail.com>
2015-08-23 13:53:17 +0200

    Automatically add the downloaded file to .gitignore

--- lib/mina/dotenv/tasks.rb
+++ lib/mina/dotenv/tasks.rb
@@ -15,6 +15,12 @@ namespace :dotenv do
   desc 'Copies the remote .env file to the current directory'
   task :pull do
     scp_download(remote_dotenv_path, dotenv_location)
+    add_to_gitignore_command = %(
+      if [[ -z $(grep #{dotenv_location} .gitignore) ]];
+        then echo #{dotenv_location} >> .gitignore;
+      fi;
+    )
+    `#{add_to_gitignore_command}`
   end
 end
 
