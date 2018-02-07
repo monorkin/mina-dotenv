265a5611aa65549a618c28dfaeb1157b12e0e35e
tzzzoz <tzzzoz@gmail.com>
2018-02-07 17:52:04 +0800

    Fix breaking changes for mina 1.x(#fetch to get local/global variables)

--- lib/mina/dotenv/tasks.rb
+++ lib/mina/dotenv/tasks.rb
@@ -4,26 +4,26 @@ require 'mina/scp'
 namespace :dotenv do
   desc 'Copies the local .env file to the server'
   task :push do
-    dotenv = Mina::Dotenv::Utils.read_file(dotenv_location)
-    queue! %(
+    dotenv = Mina::Dotenv::Utils.read_file(fetch(:dotenv_location))
+    command %{
       echo #{dotenv.shellescape} | sed -e 's/^[ \t]*//' > #{remote_dotenv_path}
-    )
-    queue! %(rm -f .env)
-    queue! %(ln -nFs #{remote_dotenv_path} .env)
+    }
+    command %{rm -f .env}
+    command %{ln -nFs #{remote_dotenv_path} .env}
   end
 
   desc 'Copies the remote .env file to the current directory'
   task :pull do
-    scp_download(remote_dotenv_path, dotenv_location)
-    add_to_gitignore_command = %(
-      if [[ -z $(grep #{dotenv_location} .gitignore) ]];
-        then echo #{dotenv_location} >> .gitignore;
+    scp_download(remote_dotenv_path, fetch(:dotenv_location))
+    add_to_gitignore_command = %{
+      if [[ -z $(grep #{fetch(:dotenv_location)} .gitignore) ]];
+        then echo #{fetch(:dotenv_location)} >> .gitignore;
       fi;
-    )
+    }
     `#{add_to_gitignore_command}`
   end
 end
 
 def remote_dotenv_path
-  "#{deploy_to}/#{shared_path}/.env"
+  "#{fetch(:shared_path)}/.env"
 end
